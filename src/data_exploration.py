5ce5a3b763abf6bc3fc5c8310d4aa80ba39c4124
Ramos-17 <gramos0617@gmail.com>
2026-04-08 01:29:53 -0700

    Configuration of previous code to proper dataset

--- src/data_exploration.py
+++ src/data_exploration.py
@@ -4,7 +4,7 @@ import numpy as np
 from pathlib import Path
 
 def load_data() -> pd.DataFrame:
-    path = Path(__file__).parent.parent /"data" / "adidas_usa.csv"
+    path = Path(__file__).parent.parent /"data" / "AdidasDataset2023to2025.csv"
     if not path.exists():
         raise FileNotFoundError(f"Missing dataset at {path}")
     return pd.read_csv(path)
@@ -22,27 +22,9 @@ def outlier_counts_using_iqr (df:pd.DataFrame) -> pd.Series:
         counts[col] = len(outliers)
     return pd.Series(counts)
 
-# Function to fill missing values in 'original_price' with 'selling_price' and convert to numeric
-def fill_missing_values(df: pd.DataFrame) -> pd.DataFrame:
-    df['original_price'] = df['original_price'].fillna(df['selling_price'])
-    df['original_price'] = pd.to_numeric(
-        df['original_price'].astype(str).str.replace('[^0-9.]', '', regex=True),
-        errors='coerce'
-    )
-    df['selling_price'] = pd.to_numeric(
-        df['selling_price'].astype(str).str.replace('[^0-9.]', '', regex=True),
-        errors='coerce'
-    )
-    return df
 
 if __name__ == "__main__":
     df = load_data()
-    print(df.head())
-    print(df.info())
-    print(df.isnull().sum())
-    print("--------------Outlier Counts using IQR Method----------------")
-    print(outlier_counts_using_iqr(df))
-    print("--------------Filling Missing Values----------------")
-    df = fill_missing_values(df)
-    print(df.isnull().sum())
+    print(df.shape)
+
     
